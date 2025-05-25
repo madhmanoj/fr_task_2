16d13043d8a3308350eaae69c391909afe2b4c2d
Madhav <madhavmanoj@outlook.com>
2025-05-25 16:01:36 +0000

    Tweaking directory structure and run script

--- run.sh
+++ run.sh
@@ -12,14 +12,20 @@ if [ ! -d "vscode" ]; then
     cd ..
 fi
 
+# Build wasm-wasi-core
+cd wasm-wasi-core
+if [ ! -d "node_modules" ]; then
+    npm install
+    npm run esbuild
+fi
+cd ..
+
 cd hello_world
 # Build the wasm files
 cargo build --target wasm32-wasip1 --target-dir ./../ext2/target
 cd ./../ext2
 if [ ! -d "node_modules" ]; then
     npm install
-else
-    echo "node_modules already exists, skipping 'npm install'"
 fi
 # # Generate TypeScript bindings from WIT interfaces using wit2ts and the Rust-defined interfaces
 # wit2ts --outDir ./src/web ./../wasm_calculator/wit
@@ -31,5 +37,5 @@ cd ..
 ./vscode/scripts/code-web.sh \
     --host 0.0.0.0 \
     --extensionDevelopmentPath=/workspaces/fr_task_2/ext2 \
-    --extensionPath=/workspaces/fr_task_2/vscode-wasm/wasm-wasi-core 
+    --extensionPath=/workspaces/fr_task_2/wasm-wasi-core 
     
