11cf9780745668939b0d7d9e1f5c22d8cfac79c6
Madhav <madhavmanoj@outlook.com>
2025-05-25 15:32:28 +0000

    Switching to wam-wasi-core extension

--- run.sh
+++ run.sh
@@ -5,27 +5,31 @@ set -e
 if [ ! -d "vscode" ]; then
     git clone https://github.com/microsoft/vscode.git
     cd vscode
-    git checkout tags/1.99.3
+    git checkout tags/1.100.0
     npm install
     npm run compile
     npm run compile-web
     cd ..
 fi
 
-cd wasm_calculator
+cd hello_world
 # Build the wasm files
-cargo build --target wasm32-unknown-unknown --target-dir ./../ext2/target
+cargo build --target wasm32-wasip1 --target-dir ./../ext2/target
 cd ./../ext2
 if [ ! -d "node_modules" ]; then
     npm install
 else
     echo "node_modules already exists, skipping 'npm install'"
 fi
-# Generate TypeScript bindings from WIT interfaces using wit2ts and the Rust-defined interfaces
-wit2ts --outDir ./src/web ./../wasm_calculator/wit
-# Remove the specific ESLint disable directive from generated files, since the package is deprecated
-find ./src/web -name '*.ts' -exec sed -i '/eslint-disable @typescript-eslint\/ban-types/d' {} +
+# # Generate TypeScript bindings from WIT interfaces using wit2ts and the Rust-defined interfaces
+# wit2ts --outDir ./src/web ./../wasm_calculator/wit
+# # Remove the specific ESLint disable directive from generated files, since the package is deprecated
+# find ./src/web -name '*.ts' -exec sed -i '/eslint-disable @typescript-eslint\/ban-types/d' {} +
 npm run compile-web
 cd ..
 
-./vscode/scripts/code-web.sh --host 0.0.0.0 --extensionDevelopmentPath=/workspaces/fr_task_2/ext2 --extensionId ms-vscode.wasm-wasi-core
+./vscode/scripts/code-web.sh \
+    --host 0.0.0.0 \
+    --extensionDevelopmentPath=/workspaces/fr_task_2/ext2 \
+    --extensionPath=/workspaces/fr_task_2/vscode-wasm/wasm-wasi-core 
+    
